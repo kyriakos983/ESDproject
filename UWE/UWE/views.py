2d07bd0e9876b986eb63bc26847273b9dc09433a
kyriakos983 <98772254+kyriakos983@users.noreply.github.com>
2022-03-28 10:38:48 +0100

    updates

--- UWE/UWE/views.py
+++ UWE/UWE/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
 
 # view the student form
 def Home(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'home.html')
