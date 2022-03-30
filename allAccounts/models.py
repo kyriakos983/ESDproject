ae15c5f905be9c0ed187cea6fba3bde7e986f308
kyriakos983 <98772254+kyriakos983@users.noreply.github.com>
2022-03-30 19:31:03 +0100

    update

--- allAccounts/models.py
+++ allAccounts/models.py
@@ -9,10 +9,5 @@ class Accounts(models.TextChoices):
 
 # Extend the  different user is it
 class User(AbstractUser):
-    # Different users of the web application for login identification
-    is_student = models.BooleanField('is_student', default=False)
-    is_clubRep = models.BooleanField('is_clubRep', default=False)
-    is_cinema_manager = models.BooleanField('is_cinema_manager', default=False)
-    is_accounts_manager = models.BooleanField('is_accounts_manager', default=False)
     accountOptions = models.CharField(max_length=50, choices=Accounts.choices, default=None, null=True, blank=True)
 
